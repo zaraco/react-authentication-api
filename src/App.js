bc5d050ec40f5c22905e0ca502f23af26c5b3bf0
Zahra Teymouri <zahra@macs-MacBook-Air.local>
2020-04-06 11:23:00 +0200

    router added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from "./Component/Login";
+import Register from "./Component/Register";
+import User from "./Component/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  render() {
+    return(
+        <div>
+            <Router>
+                <Switch>
+                <Route exact path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/user" component={User}/>
+                </Switch>
+            </Router>
+
+        </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Component/User.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1> USER </h1>
+            </div>
+        );
+    }
+}
+export default User;
