435642132607f940d5f5a4af35ad723095e62b55
Zahra Teymouri <zahra@macs-MacBook-Air.local>
2020-04-06 14:44:11 +0200

    form of login added

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import {Form,Button} from "react-bootstrap";
+
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+
 
     render() {
         return(
-            <div>
-              <h1> LOGIN</h1>
-            </div>
+
+                <Form>
+                    <input type="text"  placeholder="Enter the email address"/>
+                    <br/>
+                    <input type="text"  placeholder="Enter the password"/>
+                    <br/>
+                    <Button type="submit">Log in</Button>
+                </Form>
+
         );
     }
 }
