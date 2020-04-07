78566de01fae455603402a2295a6b270faa94f15
Zahra Teymouri <zahra@macs-MacBook-Air.local>
2020-04-07 18:52:07 +0200

    style of my site improved

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
-import {Alert} from "react-bootstrap";
-import {AlertHeading} from "react-bootstrap/Alert";
+import {Alert, Container,Row, Col, Card} from "react-bootstrap";
 
 
 class Home extends Component {
@@ -11,18 +10,33 @@ class Home extends Component {
 
     render() {
         return (
-            <>
+            <div>
 
-                    <Alert variant="info">
-                        <Alert.Heading>Hey, Welcome to my site</Alert.Heading>
-                    </Alert>
+                <Container>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <br/>
+                            <Alert variant="info">
+                                <Alert.Heading> Hey, Welcome to my site </Alert.Heading>
+                            </Alert>
+                            <br/>
+                            <Card style={{ width: '20rem', height: '20rem' }}>
+                                <Card.Img variant="top" src="download.jpeg" />
+                                <Card.Body>
+                                    <Card.Text>
+                                        we want to create the website by react and react-bootstrap..
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <br/>
+                        </Col>
+                        </Row>
+                        </Container>
+            </div>
 
-
-            </>
-
-
-        );
+    );
     }
-}
+        }
 
-export default Home;
+
+    export default Home;
